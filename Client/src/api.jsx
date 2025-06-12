1ce757f9a1a5e548c4bb4dbc77adfcc4280d72d9
Yordanos-Zegeye <yoranoszegeye24@gmail.com>
2025-06-12 16:33:44 +0300

    auth integration needs attention

---
+++ Client/src/api.jsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost:7004/api", 
+});
+
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export const signUp = (userData) => API.post("/auth/signup", userData);
+export const login = (userData) => API.post("/auth/login", userData);
+export const logout = () => API.post("/auth/logout");
+export const createPartnership = (partnershipData) =>
+  API.post("/partnership", partnershipData);
